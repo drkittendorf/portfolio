b1b2484ed6e25d735fe2a402e91419064f4ffdb7
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2021-03-22 14:26:48 -0500

    updating contact format

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,24 +1,31 @@
 import React from "react";
 import "../assets/css/style.css"
+import dale from '../assets/images/aboutDale.png';
+import "../assets/css/style.css"
 
 function contact(){
     return(
-        <div className="contact">
-       
-        
-        <h1>Contact</h1>
-        
-          <h2>           Email: drkittendorf@gmail.com                          </h2>
-          <h2>                                                       </h2>
-          <h2>LinkedIn: https://www.linkedin.com/in/dalerkittendorf/ </h2>
-          <h2>                                                       </h2>
-          <h2>GitHub: https://github.com/drkittendorf                </h2>
-          <h2>                                                       </h2>
-          <h3>             Austin Tx                                 </h3>
-            
-       
-      </div>
-    );
+        <div className="bodybody">
+     
+                <div className='sideByPair'>
+                        <div className='aboutImage'> 
+                            <img src={dale} alt="Dale Kittendorf"/> 
+                        </div> 
+
+                        <div className="aboutBody">
+                            <h1>Contact</h1>
+                
+                            <h2>           Email: drkittendorf@gmail.com               </h2>
+                            <h2>                                                       </h2>
+                            <h2>LinkedIn: https://www.linkedin.com/in/dalerkittendorf/ </h2>
+                            <h2>                                                       </h2>
+                            <h2>GitHub: https://github.com/drkittendorf                </h2>
+                            <h2>                                                       </h2>
+                            <h3>             Austin Tx                                 </h3>
+                        </div>
+                </div>
+        </div>
+   );
   }
 
 
