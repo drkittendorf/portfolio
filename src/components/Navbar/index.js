f3955ea44e58275a030ce80544f2adc6b8453369
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2021-03-23 16:35:34 -0500

    header under 500px

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,12 +7,16 @@ import './mynavbar.css'
 function Navbar() {
     return (
         <nav className='navbar'>
-            <div className='divbar'>  
-            <a href="https://github.com/drkittendorf" target="_blank" className='' 
-            rel='noopener noreferrer'><i className="fab fa-github fa-3x icon"></i></a>  
+           <div className='divbar'>  
+            <a href='https://github.com/drkittendorf' className='gittycat' 
+            target='_blank' rel='noopener noreferrer'> 
+            <i className='fab fa-github fa-3x icon'></i> 
+            </a> 
+
             <Link to={"/portfolio/about"} className="">About</Link>
             <Link to={"/portfolio/contact"} className="">Contact</Link>
             <Link to={"/portfolio/portfolio"} className="">Portfolio</Link>
+            
             <a href={resume} target="_blank" 
             rel='noopener noreferrer' className=""> Resume</a>
             
